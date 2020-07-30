9aff9ff0354f8cff7e591b995fe14d38de3105bf
Darren_Rose <thorn086@gmail.com>
2020-07-29 22:27:24 -0500

    linked lists

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,55 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null){ // next is defaulted to null
+        this.data = data; // assign the data to current data
+        this.next = next; //assign the data to the next property
+        
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor(){
+        this.head = null;
+    }
+
+    insertFirst(data){
+        this.head = new Node(data, this.head);
+    }
+
+    size(){
+        let counter = 0;
+        let node = this.head;
+
+        while(node){
+            counter ++;
+            node = node.next;
+        }
+        return counter;
+    }
+
+    getFirst(){
+        return this.head;
+    }
+
+    getLast(){
+        if (!this.head){
+            return null;
+        }
+
+        let node = this.head;
+        while(node){
+            if(!node.next){
+                return node;
+            }
+            node = node.next
+        }
+    }
+
+    clear(){
+        this.head = null;
+    }
+}
 
 module.exports = { Node, LinkedList };
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,18 +2,42 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
    return str.split('').reverse().join('');
+   
 }
 
 module.exports = reverse;
-			 //return str.split('').reduce((rev, char)=>{
-      // return char + rev;}, '');
-    //return str.split('').reverse().join(''); //solution #1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//return str.split('').reduce((rev, char)=>{
+// return char + rev;}, '');
+//return str.split('').reverse().join(''); //solution #1
 //solution 2
 // let reversed = '';
 // for (let character of str) {
